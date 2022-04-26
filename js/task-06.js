1dd4665badc0eaa79bcb4329a72b5978dcd2fab0
natalia-stebel <93938115+natalia-stebel@users.noreply.github.com>
2022-04-26 21:11:11 +0300

    done

--- js/task-06.js
+++ js/task-06.js
@@ -1,13 +1,13 @@
 const input = document.querySelector("#validation-input");
 // const inputValid = document.querySelector("#validation-input.valid");
 // const inputInvalid = document.querySelector("#validation-input.invalid");
+const inputLength = input.getAttribute("data-length")
 
 input.addEventListener("blur", inputCheck);
-
 function inputCheck(event) {
     input.classList.add("invalid")
     
-    if (event.currentTarget.value.length > input.dataset.lengt) {
+    if (event.currentTarget.value.length > inputLength) {
          
         input.classList.remove("invalid");
         input.classList.add("valid");
