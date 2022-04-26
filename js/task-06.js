b3e7af4387a729f6f6380e9eccac59ba076cec6c
natalia-stebel <93938115+natalia-stebel@users.noreply.github.com>
2022-04-26 21:56:26 +0300

    task 06

--- js/task-06.js
+++ js/task-06.js
@@ -5,12 +5,12 @@ const inputLength = input.getAttribute("data-length")
 
 input.addEventListener("blur", inputCheck);
 function inputCheck(event) {
-    input.classList.add("invalid")
+    input.classList.add("valid")
     
-    if (event.currentTarget.value.length > inputLength) {
+    if (event.currentTarget.value.length >= inputLength) {
          
-        input.classList.remove("invalid");
-        input.classList.add("valid");
+        input.classList.remove("valid");
+        input.classList.add("invalid");
     }
     
 }
