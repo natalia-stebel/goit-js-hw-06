b9fe328f023069a85c7e18bb9db2a06525162c89
natalia-stebel <93938115+natalia-stebel@users.noreply.github.com>
2022-04-26 20:29:55 +0300

    relocation files

--- js/task-09.js
+++ js/task-09.js
@@ -2,14 +2,14 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
-const changeBtnColor = document.querySelector('.change-color');
+const changeBtn = document.querySelector('.change-color');
 
 
 const bodyEl = document.querySelector('body');
 const spanEl = document.querySelector('.color');
 
 
-changeBtnColor.addEventListener('click', changeBtnClick)
+changeBtn.addEventListener('click', changeBtnClick)
 
 function changeBtnClick(){
   bodyEl.style.backgroundColor = getRandomHexColor()
